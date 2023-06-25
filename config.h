ee1a0e6567c0183ea171b505e2ea9a41adf017b4
Gustavo Meireles <gustavo@fedora>
2023-06-25 15:17:34 +0100

    Changed the font

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static int min_width = 500;                    /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"MesloLGL Nerd Font Mono:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
